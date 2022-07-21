181a811816bf1df47ca309d398cdbf357ab97884
DESKTOP-3D3QRE7\raingrains <1125089452@qq.com>
2022-07-21 15:28:03 +0800

    pbm填报修改

--- config/index.js
+++ config/index.js
@@ -1,14 +1,5 @@
 module.exports = {
-    account_list:process.env.account_list || '[]',// 账号密码数组
-    email_account:process.env.email_list ||''
+    account_list:process.env.account_list || '[{"username":"15895332950","password":"sq6QcT7Owmo+I2FZyaUGPw==","workType":"代码开发"}]',// 账号密码数组
 }
 
-/**
-{
-    leaderUserId:'131348662220926576',
-    userName:"****",
-    token:'****'
-    workType:'代码开发',
-    email:'****@qq.com'
-}
- */
+
--- index.js
+++ index.js
@@ -1,184 +1,153 @@
 const Axios = require('axios')
-const message = require('./message/index')
+const message = require('./message/email')
 const dayjs = require('dayjs')
 const config = require('./config')
 
 const axios = Axios.create({
     baseURL:'https://pbmapi.xiaoyanggroup.com/api/'
 })
-const pathList = {
-    path1:'Project/GetMainProjects',
-    path2:'WorkTime/GetWorkTimeTypes',
-    path3:'WorkTime/BatchCreateMainProjectDayWorkTime'
-}
+
+
+const accountList = JSON.parse(config.account_list)
+console.log('accountList',accountList)
 
 init()
+
+
 async function init(){
-    
-    const accountList = JSON.parse(config.account_list)
-//     console.log(accountList);
 
     // 接入接口坞的接口 判断当前是否是工作日 地址：http://www.apihubs.cn/#/holiday
   const isWorkDay =await axios.get(`https://api.apihubs.cn/holiday/get?field=workday&date=${dayjs(new Date()).format('YYYYMMDD')}&workday=1&cn=1&size=31`).then(res=>{
       return res.data.data.list.length >0
   })
-  console.log('今天不是工作日，不需要填报晓羊PBM哦~');
  
   // 如果不是工作日，则不执行下面的语句
-  if (!isWorkDay) return
+  if (!isWorkDay) {
+    console.log('今天不是工作日，不需要填报晓羊PBM哦~');
+    return
+  }
 
-  console.log('今天是工作日，需要填报晓羊PBM哦~');
- 
-    if(accountList.length>0){
-        const arr = []
-        accountList.forEach(item=>{
-            main(item)
-            arr.push(main(item))
-        })
-
-        message.sendMail({
-            email:'raingrains@foxmail.com',
-            text:`
-            ${dayjs(new Date()).format('YYYY年MM月DD日')}晓羊PBM脚本运行结果:
-                ${JSON.stringify(arr)}
-            `,
-            subject:`${dayjs(new Date()).format('YYYY年MM月DD日')}晓羊PBM脚本运行结果`
-        })
-        
-    }else{
-        message.sendMail({
-            email:'raingrains@foxmail.com',
-            text:`
-            晓羊PBM自动填写的github仓库需要配置account_list 格式为
-    
-            [
-                {
-                    leaderUserId:'领导的userId',
-                    token:'自己的账号token', //8ab6d1d675cefc7dc39e86c7c02a4b65
-                    workType:'代码开发', // 自己的工作类别
-                    email:'消息提醒的邮箱账号'
-                }
-            ]
-            `
-        })
-    }
-    
+//   console.log('今天是工作日，需要填报晓羊PBM哦~',new Date(dayjs(new Date()).format('YYYY-MM-DD HH:mm:ss')).getTime());
+  
+  accountList.forEach(item=>{
+    handle(item)
+  })
 }
 
 
+async function handle(user){
+   
+    // 获取用户id和token
+    const {token,UserID} = await axios.post('https://pbmapi.xiaoyanggroup.com/xy/login/Mobile',{
+        "password": user.password,
+        "loginId": user.username,
+        "icon": ""
+    }).then(res=>{
+        // console.log(res.data);
+        return res.data.data
+    })
 
- async function main(user){
-    
-    const state = {
-        selfProjectList:[],
-        workTypeList:[],
-        status:'error',
-        message:null
-    }
-   const taskList = await  axios.get(pathList.path1,{
-        params:{
-            AscOrderById:false,
-            page:1,
-            SearchKey:'',
-            status:'',
-            ProjectType:""
-        },
+    // console.log('token',token,'UserID',UserID)
+
+    // 获取待完成任务列表
+    const taskList = await axios.get(`https://pbmapi.xiaoyanggroup.com/api/IterationTask/GetLatelyTaskList`,{
         headers:{
-            xytoken:user.token
+            xytoken:token
         }
     }).then(res=>{
-        return res.data.data.data
-    }).catch((err)=>{
-        message.sendMail({
-            email:user.email,
-            text: err
+        // console.log('taskListRes',res.data.data)
+
+        return res.data.data.filter(item=>{
+            return new Date().getTime() > new Date(item.PlanStartTime).getTime() && new Date().getTime() < new Date(item.PlanEndTime).getTime()
         })
-        state.message = err
-//         if(err){
-//             message.sendMail({
-//                 email:user.email,
-//                 text: 'token信息已过期,请重新登陆'
-//             })
-//         }else{
-//             message.sendMail({
-//                 email:user.email,
-//                 text: err
-//             })
-//         }
-        return false
     })
-    
 
+    // console.log('taskList',taskList)
 
-    if (!taskList) {
-        return {
-            user:user.userName,
-            status:state.status,
-            message:state.errorMessage
-        }
-    }
+    let content = ''
+    taskList.forEach(async item=>{
+       await submitTask(item,token,user.workType)
+       content += `您的${item.TaskContent}任务，已填报完成，请您知晓！\n`
+    })
 
-    state.selfProjectList = taskList.filter(item=>item.ProjectDirectorUserId===user.leaderUserId&&item.ProjectStatus===1)
+    console.log('content',content)
+    // 消息推送
+    axios.post('http://www.pushplus.plus/send',{
+        "token":"cc966b4a88f14046847ab848a12e4aeb",
+        "title":"晓羊PBM填报结果",
+        "content":content,
+        "templete":'html'
+    }).then(res=>{
+        console.log('消息推送结果',res.data)
+    })
+}
 
-    // 获取工作类别的列表数据
-    state.workTypeList  = await axios.get(pathList.path2,{
+async function submitTask(task,token,workType){
+    // 获取任务详情
+  let taskDetail =  await axios.get('https://pbmapi.xiaoyanggroup.com/api/IterationTask/GetTaskDetail',{
         params:{
-            projectId:state.selfProjectList[0].ProjectId
+            'TaskId':task.Id
         },
         headers:{
-            xytoken:user.token
+            xytoken:token
         }
     }).then(res=>{
-        return res.data.data.find(item=>item.FunctionName==="产品研发部").Items
+        // console.log('taskDetail:',res.data.data);
+        return res.data.data
     })
 
-    const taskCompleteObj = {
-        ProjectId:state.selfProjectList[0].ProjectId,
-        WorkTimeTypeName:user.workType,
-        fillInTime:8,
-        key: "",
-        workDes: "",
-        workType: state.workTypeList.find(item=>item.WorkTimeTypeName===user.workType).WorkTimeTypeId
-    }
-    
-    // 提交PBM传参
-    const data = {
-        fillInDateTime: dayjs(new Date()).format('YYYY-MM-DD'),
-        items:[taskCompleteObj],
-        remarks:''
-    }
-
-
-    // 填报成功的消息提示
-    const tipsText  = `
-        您今日的项目【${state.selfProjectList[0].ProjectName}】的工时类别【${taskCompleteObj.WorkTimeTypeName}】已进行8小时的填报
-    `
-
-     await axios.post(pathList.path3,data,{
+    // 获取工作类别
+    let workTypes = await axios.get('https://pbmapi.xiaoyanggroup.com/api/WorkTime/GetWorkTimeTypes',{
+        params:{
+            'projectId':taskDetail.ProjectId
+        },
         headers:{
-            xytoken:user.token
+            xytoken:token
         }
     }).then(res=>{
-        console.log(res.data);
-        if(res.data.data===0){
-            message.sendMail({
-                email:user.email,
-                text:  res.data.msg
-            })
-            state.message = res.data.msg
-            state.status = 'error'
-        }else{
-            message.sendMail({
-                email:user.email,
-                text:  tipsText
-            })
-            state.message =  tipsText
-            state.status = 'success'
-        }
+        return res.data.data.map(item=>item.Items).flat(Infinity)
     })
-    return {
-        user:user.userName,
-        status:state.status,
-        message:state.message
-    }
+
+    // console.log('workTypes',workTypes)
+
+    const workTypeId = workTypes.find(item=>item.WorkTimeTypeName===workType||'代码开发').WorkTimeTypeId
+
+    console.log('任务状态',{
+        title:taskDetail.TaskContent,
+        "fillInDateTime": dayjs(new Date()).format('YYYY-MM-DD'),
+        "projectId": taskDetail.ProjectId,
+        "fillInTime": taskDetail.WorkTimeEstimate,
+        "workType": workTypeId,
+        "workDes": "",
+        "taskId": taskDetail.Id,
+        "taskState": dayjs(new Date()).format("YYYY-MM-DD") === dayjs(taskDetail.PlanEndTime).format("YYYY-MM-DD") ? 3: 2
+    })
+
+    
+
+    return 
+
+    // 提交PBM填报任务
+    await axios.post('https://pbmapi.xiaoyanggroup.com/api/WorkTime/BatchCreateMainProjectDayWorkTime',{
+        remarks:'',
+        Items:[
+            {
+                "fillInDateTime": dayjs(new Date()).format('YYYY-MM-DD'),
+                "projectId": String(taskDetail.ProjectId),
+                "fillInTime": taskDetail.WorkTimeEstimate,
+                "workType": workTypeId,
+                "workDes": "",
+                "taskId": String(taskDetail.Id),
+                "taskState": dayjs(new Date()).format("YYYY-MM-DD") === dayjs(taskDetail.PlanEndTime).format("YYYY-MM-DD") ? 3: 2
+            }
+        ]
+    },{
+        headers:{
+            xytoken:token
+        }
+    }).then(res=>{
+        console.log('填报完成-接口返回',res)
+    })
+
+    
 }
