27ac167ffe116c646e0fe1284aa6e4fb98048dc0
raingrains <1125089452@qq.com>
2022-12-02 16:41:44 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    account_list:process.env.account_list || '[]',// 账号密码数组
+    account_list:process.env.account_list || '[]',// 账号密码数组[{"password":"","username":"","icon":""}]
     pushplus_token:process.env.pushplus_token || '',// pushPlus的token
 }
 
